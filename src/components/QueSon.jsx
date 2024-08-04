b4d47631658dc41afd2c8f91331cebd07bac1e86
100495754 <100495754@alumnos.uc3m.es>
2024-08-04 17:33:18 +0200

    avances en que son

--- src/components/QueSon.jsx
+++ src/components/QueSon.jsx
@@ -4,6 +4,40 @@ import Navbar from "./Navbar";
 import AnimatedText from "./AnimatedText";
 import Footer from "./Footer";
 
+
+
+const avatars_diagnosis = [
+  {
+    src: "./rmn.png",
+    alt: "Avatar",
+    size: "30vh",
+    name: "Resonancia Magnética (RMN)",
+    role: "Presidenta",
+  },
+  {
+    src: "./tac.png",
+    alt: "Avatar",
+    size: "30vh",
+    name: "Tomografía Axial Computarizada (TAC)",
+    role: "Vocal",
+  },
+  {
+    src: "./emg.png",
+    alt: "Avatar",
+    size: "30vh",
+    name: "Electromiografía (EMG)",
+    role: "Tesorera",
+  },
+  {
+    src: "./mielografia.png",
+    alt: "Avatar",
+    size: "30vh",
+    name: "Mielografía",
+    role: "Vocal",
+  },
+];
+
+
 function QueSon() {
   return (
     <>
@@ -12,7 +46,7 @@ function QueSon() {
         <AnimatedText as="h1" text="¿QUÉ SON LOS QUISTES DE TARLOV?" />
         <section className="presentation" id="#¿que-son-los-quistes-de-tarlov">
           <Avatar
-            src="../src/assets/isadore-tarlov.jpg"
+            src="/isadore-tarlov.jpg"
             alt="Avatar"
             size="30vh"
           />
@@ -46,22 +80,27 @@ function QueSon() {
               <br />
             </p>
             <div className="layout">
-              <p>
-                Se pueden dar a cualquier nivel de la columna vertebral,
-                normalmente entre las dos de las tres meninges que recubren la
-                médula espinal, aunque es frecuente que su localización sea en
-                la zona sacra. <br />
+              <div className="">
+                <p>
+                  Se pueden dar a cualquier nivel de la columna vertebral,
+                  normalmente entre las dos de las tres meninges que recubren la
+                  médula espinal, aunque es frecuente que su localización sea en
+                  la zona sacra. <br />
+                  <br />
+                  <b>
+                    Es habitual su presencia en la población general, aunque se
+                    estima que solo entre un 5 y un 9% de la población presenta
+                    síntomas relacionados con los mismos.
+                  </b>
+                </p>
                 <br />
-                <b>
-                  Es habitual su presencia en la población general, aunque se
-                  estima que solo entre un 5 y un 9% de la población presenta
-                  síntomas relacionados con los mismos.
-                </b>
-              </p>
-              <img src="../src/assets/radiografia.jpg" alt="" />
+                <h3>No es considerada una enfermedad rara desde 2024, pero sí poco frecuente</h3>
+              </div>
+              <img src="/radiografia.jpg" alt="" />
             </div>
           </div>
         </section>
+
         <section className="symptoms" id="#sintomatologia">
           <h3>Síntomas</h3>
           <div className="content">
@@ -93,6 +132,67 @@ function QueSon() {
             </ul>
           </div>
         </section>
+
+        <section className="symptoms" >
+          <h3> Otros Síntomas</h3>
+          <div className="content">
+          <ul>
+            <li>Dolor crónico lumbar, sacro, coccígeo y radicular.</li>
+            <li>Hormigueos o sensaciones anómalas en las piernas.</li>
+            <li>Dolor o sensaciones anómalas en la zona pélvica o perineal</li>
+            <li>Falta de fuerza en las piernas</li>
+            <li>Disfunción sexual, urinaria o defecatoria.</li>
+            <li>Infecciones de orina</li>
+            <li>Dificultad en la continencia gaseosa.</li>
+            <li>Signos de hipotensión del líquido cefalorraquídeo (dolor de cabeza que aparece cuando el paciente se pone de pie).</li>
+            <li>Dolor en bipedestación (de pie), sedestación (sentado), o caminando</li>
+            <li>Dolor pélvico</li>
+            <li>Dolor en relaciones sexuales (dispareunia)</li>
+          </ul>
+
+          </div>
+        </section>
+
+        <section className="causas" id="#causas">
+          <h3> Causas</h3>
+          <div className="content">
+            <p><b>Actualmente se desconocen las causas,</b> aunque se barajan varias hipótesis: <br /></p>
+          <ul>
+            <li>Dolor crónico lumbar, sacro, coccígeo y radicular.</li>
+            <li>Hormigueos o sensaciones anómalas en las piernas.</li>
+            <li>Dolor o sensaciones anómalas en la zona pélvica o perineal</li>
+            <li>Falta de fuerza en las piernas</li>
+          </ul>
+          <p>Surgen por el aumento de la presión del líquido cefalorraquídeo junto con un fenómeno valvular anómalo que permite el no retorno del líquido que se encuentra dentro del quiste.</p>
+
+          </div>
+        </section>
+
+        <section className="diagnosis" id="#diagnostico">
+          <h3>
+            Diagnóstico
+          </h3>
+
+          <div className="content"> 
+          {avatars_diagnosis.map((avatar, index) => (
+          <div className={`display${index}`} key={index}>
+            <Avatar
+            key={index}
+            src={avatar.src}
+            alt={avatar.alt}
+            size={avatar.size}
+            delay={index * 500}
+          />
+          <h3>{avatar.name}</h3>
+          <p>{avatar.role}</p>
+          </div>
+        ))}
+        
+
+          </div>
+
+        </section>
+
       </div>
 
       <Footer></Footer>
